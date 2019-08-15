7c1a529a0c63d3429bfb8f197e1494bc0320b686
iyytdeed <849147703@qq.com>
2019-08-15 22:17:24 +0800

    Add files via upload

---
+++ Part5_RNN Based AMC/Test/debug/test.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+from matplotlib import pyplot as plt
+plt.style.use("ggplot")
+
+import json
+import numpy as np
+import scipy.io as sio
+
+from keras import backend as K
+from keras.models import model_from_json
+from keras.layers import Dense, Dropout, Activation, Flatten, Embedding, LSTM, GRU, Input, RepeatVector, TimeDistributed, Lambda, Reshape
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+
+# load model
+model = model_from_json(json.load(open("lstm_model_struct.json")))
+
+# load wights
+model.load_weights("./lstm_model_struct.h5")
+model.summary()
+
+# test begin
+nClass = 4
+seqLen = 100
+nb = 1000
+EsNoLow = -10
+EsNoHigh = 20
+Gap = 2
+pred_mat = np.zeros(((EsNoHigh-EsNoLow)/Gap+1, nClass, nClass))
+# load test data
+data_mat = sio.loadmat('./dataset/test_data.mat')
+data_complex = data_mat['test_data']
+data_real = data_complex.real
+data_imag = data_complex.imag
+EsNoArray = data_real[:,-1]
+y_test = data_real[:,-2]
+data_real = data_real[:,0:seqLen]
+data_imag = data_imag[:,0:seqLen]
+data_real = data_real.reshape((data_real.shape[0], seqLen))
+data_imag = data_imag.reshape((data_imag.shape[0], seqLen))
+x_test = np.stack((data_real, data_imag), axis=2)
+y_predict = model.predict(x_test)
+
+# get predict result
+for i in range(y_test.shape[0]):
+    axis_0 = (int)((EsNoArray[i] - EsNoLow)/Gap)
+    # should be
+    axis_1 = (int)(y_test[i])
+    # predict to be
+    axis_2 = np.argmax(y_predict[i,:])
+    pred_mat[axis_0, axis_1, axis_2] = pred_mat[axis_0, axis_1, axis_2] + 1
+saveFileName  = "pred_confusion_mat_L" + str(seqLen)
+np.save(saveFileName, pred_mat)
---
+++ Part5_RNN Based AMC/Train/debug/train_GRU.py
@@ -0,0 +1,76 @@
+import numpy as np
+import tensorflow as tf
+import random as rn
+import os
+import json
+
+from keras import metrics, regularizers, optimizers, backend
+from keras.callbacks import TensorBoard, EarlyStopping
+from keras.models import Model
+from keras.layers import Input, Dense, LSTM, CuDNNLSTM, CuDNNGRU, Flatten, Dropout, BatchNormalization
+from keras.utils import np_utils, vis_utils
+
+import scipy.io as sio
+
+# fix random seed
+os.environ['PYTHONHASHSEED'] = '0'
+np.random.seed(2018)
+rn.seed(2018)
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=4)
+tf.set_random_seed(2018)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+backend.set_session(sess)
+
+seqLen = 100
+nClass = 4
+samNum = 100000 * nClass
+# load train data
+x_data_mat = sio.loadmat('./dataset/train_data.mat')
+x_data_complex = x_data_mat['train_data']
+x_data_real = x_data_complex.real
+x_data_imag = x_data_complex.imag
+x_data_real = x_data_real.reshape((x_data_real.shape[0], seqLen))
+x_data_imag = x_data_imag.reshape((x_data_imag.shape[0], seqLen))
+x_train = np.stack((x_data_real, x_data_imag), axis=2)
+y_data_mat = sio.loadmat('./dataset/train_label.mat')
+y_data = y_data_mat['train_label']
+y_train = np_utils.to_categorical(y_data, nClass)
+# train data shuffle
+index = np.arange(y_train.shape[0])
+np.random.shuffle(index)
+x_train = x_train[index,:]
+y_train = y_train[index]
+
+_in_ = Input(shape = (x_train.shape[1], x_train.shape[2]))
+ot = CuDNNGRU(16, return_sequences=False)(_in_)
+#ot = Flatten()(ot)
+#ot = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001, center=True, scale=True)(ot)
+#ot = Dense(64, use_bias=True, activation='relu')(ot)
+ot = Dense(16, use_bias=True, activation='relu')(ot)
+_out_ = Dense(nClass, activation='softmax')(ot)
+model = Model(_in_, _out_)
+
+tensor_board = TensorBoard(log_dir='./tensorboard_log', histogram_freq=0, write_graph=True, write_images=False,
+                            embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+early_stopping = EarlyStopping(monitor='val_loss', patience=5)
+adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+model.compile(loss='categorical_crossentropy',
+               optimizer=adam, 
+               metrics=['categorical_accuracy'])
+model.fit(x_train,
+          y_train, 
+          epochs=100, 
+          batch_size=100,
+          validation_split=0.1,
+          shuffle=True,
+          #callbacks=[])
+          #callbacks=[tensor_board])
+          #callbacks=[early_stopping])
+          callbacks=[tensor_board, early_stopping])
+scores = model.evaluate(x_train, y_train)
+print(" %s %f" % (model.metrics_names[1], scores[1]))
+model.summary()
+with open('gru_model_struct.json', 'w') as f:
+    json.dump(model.to_json(), f)    
+model.save_weights('gru_model_struct.h5')
+
